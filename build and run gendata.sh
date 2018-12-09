69804bc5318242c457184fadad536625e86846cb
Alan Olivares <alanolivares@MacBook-Pro-de-Alan.local>
2018-12-08 21:19:32 -0300

    more

--- build and run gendata.sh
+++ build and run gendata.sh
@@ -12,7 +12,7 @@ make clean
 make -f Makefile.suite
 
 #For generating 1TB, execute following command
-nohup ./dsdgen -scale 1000 -verbose Y -FORCE -dir tmp1000 &
+nohup ./dsdgen -scale 1000 -verbose Y -terminate n -FORCE -dir tmp1000 &
 
 nohup gsutil -m  cp *.dat gs://aol-temp/gendata1000 &
 
@@ -22,6 +22,10 @@ nohup gsutil -m  cp *.dat gs://aol-temp/gendata1000 &
 sudo apt-get update
 sudo apt-get -y --force-yes install gcc make flex bison byacc git
 make clean
-make -f makefile.suite
+make -f makefile
 
-nohup ./dbgen -s 100 -v Y -f &
+#For generating 10TB, execute following command
+nohup ./dbgen -s 10000 -v Y -f &
+
+
+nohup sudo gsutil -m cp *.tbl gs://aol-temp/gendata_tpch_100 &
